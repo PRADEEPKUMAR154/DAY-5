23f769cc5742f9f53c0c54650dc4436d3a851612
PRADEEPKUMAR <pradeepmbiloor@gmail.com>
2022-06-22 18:56:33 +0530

    day 5 problems

---
+++ Vowel.java
@@ -0,0 +1,21 @@
+public class Vowel
+{
+
+    public static void main(String[] args)
+    {
+
+        char alphabet='t';
+
+        if(alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u')
+        {
+            System.out.println("Given Aplbhabet :"+alphabet+" is a vowel");
+        }
+        else
+        {
+            System.out.println("Given Aplbhabet :"+alphabet+" is a Consonant");
+
+        }
+
+    }
+
+}
---
+++ harmonicno.java
@@ -0,0 +1,29 @@
+public class harmonicno {
+
+    public static void main(String[] args) {
+
+        int nValue=5;
+        int numerator,denominator;
+        double harmonicValue=0;
+
+        numerator=1;
+
+        if(nValue!=0)
+        {
+            for(int index=1;index<=nValue;index++)
+            {
+                denominator=index;
+                harmonicValue=harmonicValue+(double)numerator/denominator;
+
+            }
+
+            System.out.println("Nth Harmonic value is: "+harmonicValue);
+        }
+        else
+        {
+            System.err.println("N value cannot be 0");
+        }
+
+    }
+
+}
